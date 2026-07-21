fd791dbfa81921f9de03f6bda30e0aa47f1ee76f
LiliSeedHunter <liliseedhunter@gmail.com>
2026-07-21 13:09:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Story engine loaded");
+fetch("story.json")
+    .then(response => response.json())
+    .then(story => {
+
+        const container = document.getElementById("story");
+
+        story.forEach(scene => {
+
+            const block = document.createElement("section");
+
+            block.className = "scene";
+
+            block.innerHTML = `
+                <p class="caption">
+                    ${scene.text}
+                </p>
+
+                <img src="images/${scene.image}">
+            `;
+
+            container.appendChild(block);
+
+        });
+
+    });
